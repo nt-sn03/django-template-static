b04ed640c05429c31e62de5a374f0e1c4c8bcfb4
djumanov <djumanovdev@gmail.com>
2026-01-10 08:53:51 +0500

    urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def home_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'home.html')
+
+
+def about_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'about.html')
+
+
+def blog_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'blog.html')
+
+
+def blog_detail_view(request: HttpRequest, blog_id: int) -> HttpResponse:
+    return render(request, 'blog_detail.html')
+
+
+def contact_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'contact.html')
