45c80383bfe55dac1d95ac2f35172bce84660909
juraqulovazizbek <juraqulovazizbek85@gmail.com>
2026-01-13 08:33:25 +0500

    takrorlash

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import home_page
+from .views import home_page , projects_page , inbox_page , analytics_page , settings_page
 
 
 urlpatterns = [
     path('', home_page, name='home_page'),
+    path('projects/', projects_page , name='projects_page'),
+    path('inbox/', inbox_page, name='inbox_page'),
+    path('analytics/', analytics_page, name='analytics_page'),
+    path('settings/', settings_page, name='settings_page')
 ]
