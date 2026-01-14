25215b14de132e68ec5a42669c1affca73545ed4
juraqulovazizbek <juraqulovazizbek85@gmail.com>
2026-01-14 11:48:57 +0500

    url name

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,13 @@ def home_view(request: HttpRequest) -> HttpResponse:
 
 
 def about_view(request: HttpRequest) -> HttpResponse:
-    return render(request, 'about.html')
+    data={
+        'first_name': 'Azizbek',
+        'last_name': 'Juraqulov',
+        'phone': '+998-94-566-58-66',
+        'email': 'juraqulovazizbek@gmail.com',
+    }
+    return render(request, 'about.html', context=data)
 
 
 def blog_view(request: HttpRequest) -> HttpResponse:
